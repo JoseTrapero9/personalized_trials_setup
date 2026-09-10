22d038096238669229d4e97a1f56288629407610
fabianfhers25 <fabianfhers25@gmail.com>
2026-09-10 11:52:34 +0200

    aaa

--- robot_sequence.py
+++ robot_sequence.py
@@ -117,7 +117,7 @@ class Scripter:
     def move_left_down(self, wait_time: float = 2.00):
                 # define your down position in degrees
                 degrees = [128.6, -46.09, 68.87, -111.79, -87.97, -51.47] 
-                self._send_movej_degrees(degrees, delay=wait_time)
+                self._send_movel_degrees(degrees, delay=wait_time)
 
     def move_right_up(self, wait_time: float = 2.00):
             # define your down position in degrees
@@ -127,7 +127,7 @@ class Scripter:
     def move_right_down(self, wait_time: float = 2.00):
             # define your down position in degrees
             degrees = [157.92, -68.53, 107.44, -128.70, -87.78, -21.95] 
-            self._send_movej_degrees(degrees, delay=wait_time)
+            self._send_movel_degrees(degrees, delay=wait_time)
 
     def move_ramp_disposal_up(self, wait_time: float = 2.00):
             # define your down position in degrees
@@ -137,7 +137,7 @@ class Scripter:
     def move_ramp_disposal_down(self, wait_time: float = 2.00):
             # define your down position in degrees
             degrees = [181.04, -95.01, 114.02, -108.08, -89.59, 91.58] 
-            self._send_movej_degrees(degrees, delay=wait_time)
+            self._send_movel_degrees(degrees, delay=wait_time)
 
     def close(self):
         # clean up script socket
@@ -177,26 +177,29 @@ def main():
             # Going to first collection point
             s.move_left_up(wait_time=5)
             wait_for_gui_signal()
-            s.move_left_down(wait_time=5)
+            s.move_left_down(wait_time=2.5)
             s.close_gripper()
             #going to the ramp
-            s.move_left_up(wait_time=5)
-            s.move_ramp_disposal_up(wait_time=2)
-            s.move_ramp_disposal_down(wait_time=3)
+            s.move_left_up(wait_time=2.5)
+            s.move_ramp_disposal_up(wait_time=5)
+            s.move_ramp_disposal_down(wait_time=2.5)
+            
             s.open_gripper()
+            s.move_ramp_disposal_up(wait_time=5)
 
             s.move_home(wait_time=5)
             
             # Going to second collection point
             s.move_right_up(wait_time=5)
             wait_for_gui_signal()
-            s.move_right_down(wait_time=5)
+            s.move_right_down(wait_time=2.5)
             s.close_gripper()
             #going to the ramp
-            s.move_left_up(wait_time=5)
-            s.move_ramp_disposal_up(wait_time=2)
-            s.move_ramp_disposal_down(wait_time=3)
+            s.move_right_up(wait_time=2.5)
+            s.move_ramp_disposal_up(wait_time=4)
+            s.move_ramp_disposal_down(wait_time=2.5)
             s.open_gripper()
+            s.move_ramp_disposal_up(wait_time=2.5)
 
 
             
